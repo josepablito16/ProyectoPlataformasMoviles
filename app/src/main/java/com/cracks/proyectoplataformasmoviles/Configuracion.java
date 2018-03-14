716d7a30008c0d924f4b1b323eb3bb12e4dfa69a
javiercarpio57 <javierrr.carpio57@gmail.com>
2018-03-14 09:27:54 -0600

    modificaciones

--- app/src/main/java/com/cracks/proyectoplataformasmoviles/Configuracion.java
+++ app/src/main/java/com/cracks/proyectoplataformasmoviles/Configuracion.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Configuracion extends AppCompatActivity {
     private static final int PICK_IMAGE = 100;
@@ -39,22 +40,26 @@ public class Configuracion extends AppCompatActivity {
         TextView nombreUsuario = findViewById(R.id.usuarioTV);
         nombreUsuario.setText("¡Bienvenido " + usuario + "!");
 
-        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal){
-                number.setText(newVal);
-            }
-        });
-
         foto_gallery = (ImageView)findViewById(R.id.imageView);
         boton = (Button)findViewById(R.id.btnImage);
         boton1 = (Button)findViewById(R.id.btnNext);
         number = (TextView)findViewById(R.id.txtNumber);
 
+        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal){
+                Toast.makeText(getApplicationContext(), "" + newVal,Toast.LENGTH_LONG).show();
+                number.setText("");
+                number.setText("" + newVal);
+            }
+        });
+
+
+
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Configuracion.this, SalaEspera.class);
+                Intent intent = new Intent(Configuracion.this, Matriz.class);
                 intent.putExtra("persona", number.getText());
                 startActivityForResult(intent, 1);
             }
