8c6f81c808caa5e1d3d3b761d8a31ddc2218eb1a
OJP98 <ojuarez_p@hotmail.com>
2018-03-20 07:57:02 -0600

    Unos cambios

--- app/src/main/java/com/cracks/proyectoplataformasmoviles/Display.java
+++ app/src/main/java/com/cracks/proyectoplataformasmoviles/Display.java
@@ -1,12 +1,15 @@
 package com.cracks.proyectoplataformasmoviles;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -90,6 +93,19 @@ public class Display extends AppCompatActivity {
         setContentView(R.layout.activity_display);
 
         mVisible = true;
+
+        final ImageView imagen =  findViewById(R.id.display_IV);
+
+        int filas = 1;
+        int columnas = 2;
+        Bitmap i = BitmapFactory.decodeResource(getResources(), R.drawable.fondo2);
+        int height = i.getHeight()/filas;
+        int width = i.getWidth()/columnas;
+        int posx = 0;
+        int posy = 0;
+        i = Bitmap.createBitmap(i,posx*width,posy*height,width,height);
+        imagen.setImageBitmap(Bitmap.createScaledBitmap(i,i.getWidth()*2,i.getHeight(),true));
+        imagen.setImageBitmap(Bitmap.createScaledBitmap(i,imagen.getWidth(),imagen.getHeight(),true));
     }
 
     @Override
